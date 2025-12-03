067648684870197574f76d36b443280a65cbb62f
scillidan <scillidan@users.noreply.github.com>
2025-12-03 17:50:33 +0000

    deploy: 5471c61196798dbcbcb376ced4e8ec9162e83baa

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-67cef739.js',
+                path_to_root + 'searchindex-32e01ba8.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
