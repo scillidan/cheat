b08cf59196d09bdd219d3ad2ac50415908ba1485
scillidan <scillidan@users.noreply.github.com>
2025-12-24 16:33:27 +0000

    deploy: 0fa9c35b109395a0192b9f2dcc75db4e6ef63fc1

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-d8c89d3f.js',
+                path_to_root + 'searchindex-49a2153b.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
